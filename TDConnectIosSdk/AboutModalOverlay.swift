b07774c8d2e938341f784198ca1fe26a522ae8d5
Serhii Bovtriuk <serhii.bovtriuk@telenordigital.com>
2019-10-22 10:11:33 +0200

    Add "Learn more" button
    - Adding "Learn more" button
    - About screen is now called from inside code
    - Added fonts handling

--- TDConnectIosSdk/AboutModalOverlay.swift
+++ TDConnectIosSdk/AboutModalOverlay.swift
@@ -17,7 +17,6 @@ public class AboutModalOverlay: UIView {
     
     let nibName = "AboutModalOverlay"
     var contentView: UIView!
-    var timer: Timer?
         
     public override init(frame: CGRect) {
         // For use in code
@@ -31,6 +30,7 @@ public class AboutModalOverlay: UIView {
         setUpView()
     }
     
+    // In case if fonts are missing or namings are different
     public func provideFonts(titleFont: UIFont?, subtitleFont: UIFont?, descriptionFont: UIFont?) {
         if (titleFont != nil) {
             title.font = titleFont
@@ -46,30 +46,24 @@ public class AboutModalOverlay: UIView {
     private func setUpView() {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: self.nibName, bundle: bundle)
-        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        self.contentView = (nib.instantiate(withOwner: self, options: nil).first as! UIView)
         addSubview(contentView)
         
         self.contentView.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.width, height: self.frame.height)
-
+        
         title.text = "What is Telenor ID?"
         title.lineBreakMode = .byWordWrapping
+        title.font = UIFont(name: "Telenor-Bold", size: 24)
         
         subtitle.text = "With a single Telenor ID you easily access your apps and services.";
         subtitle.lineBreakMode = .byWordWrapping
+        subtitle.font = UIFont(name: "Telenor", size: 17)
         
         contentDescription.text = "The sign-in process has additional layers of protection, such as verifying your mobile number and email. Telenor ID gives you full control of your personal data, while keeping it safe.";
         contentDescription.lineBreakMode = .byWordWrapping
+        contentDescription.font = UIFont(name: "Telenor-Light", size: 17)
         
         closeButton.setImage(UIImage(named: "icon-close.png"), for: .normal)
-        
-        for family: String in UIFont.familyNames
-        {
-            print(family)
-            for names: String in UIFont.fontNames(forFamilyName: family)
-            {
-                print("== \(names)")
-            }
-        }
     }
     
     @IBAction func closePopup(_ sender: Any) {
