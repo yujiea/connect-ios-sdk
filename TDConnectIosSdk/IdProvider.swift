663dfab76783f67bc63cea153b9979c2061d5cd8
Serhii Bovtriuk <bovtriuk@gmail.com>
2019-10-24 12:54:18 +0200

    Add support for GP ID (#67)
    * Add support for GP ID
    
    - Add GP ID option to idProvider
    - Refactor staging urls
    
    * Add namings to the brands

--- TDConnectIosSdk/IdProvider.swift
+++ TDConnectIosSdk/IdProvider.swift
@@ -9,13 +9,29 @@
 import Foundation
 
 public enum IdProvider {
-    case connectId, telenorId
+    
+    case connectId, telenorId, gpId
+    
+    func getName() -> String {
+        switch self {
+        case .connectId:
+            return "CONNECT";
+        case .telenorId:
+            return "Telenor ID";
+        case .gpId:
+            return "GP ID";
+        }
+    }
+    
     func getUrl(useStaging: Bool) -> String {
         switch self {
             case .connectId:
                 return useStaging ? "https://connect.staging.telenordigital.com/oauth" : "https://connect.telenordigital.com/oauth";
             case .telenorId:
-                return useStaging ? "https://signin.staging-telenorid.com/oauth" : "https://signin.telenorid.com/oauth";
+                return useStaging ? "https://signin.telenorid-staging.com/oauth" : "https://signin.telenorid.com/oauth";
+            case .gpId:
+                return useStaging ? "https://signin.gp-id-staging.com/oauth" : "https://signin.gp-id.com/oauth";
         }
     }
+    
 }
